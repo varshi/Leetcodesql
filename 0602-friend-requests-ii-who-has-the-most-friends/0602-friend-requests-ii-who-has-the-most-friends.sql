ab05931e6aa90625271401d8100042c00fbc856a
varshi <53485471+varshi@users.noreply.github.com>
2025-06-25 23:55:45 +0530

    Time: 292 ms (84.36%), Space: 0B (100%) - LeetHub

---
+++ 0602-friend-requests-ii-who-has-the-most-friends/0602-friend-requests-ii-who-has-the-most-friends.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+-- `union` selects only unique vals, so we use `union all` here
+
+WITH CTE AS (
+    SELECT requester_id AS id FROM RequestAccepted
+    UNION ALL
+    SELECT accepter_id AS id FROM RequestAccepted
+)
+
+SELECT id, COUNT(id) AS num
+FROM CTE
+GROUP BY id
+ORDER BY num DESC
+LIMIT 1
